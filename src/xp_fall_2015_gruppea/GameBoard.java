bb55ab0db382487557ce46fb7d2d06a0b31f4024
Elinor Mohr <elinor.m.mikkelsen@gmail.com>
2015-10-26 19:15:10 +0100

    Sat op til netbeans

---
+++ src/xp_fall_2015_gruppea/GameBoard.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xp_fall_2015_gruppea;
+
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Elinor-skole
+ */
+public class GameBoard {
+    public GameBoard(){
+        Stage gameStage = new Stage();
+        VBox gameBox = new VBox();
+        
+        TextArea chatTextArea = new TextArea();
+        chatTextArea.setEditable(false);
+        ScrollPane chatTextAreaScroll = new ScrollPane(chatTextArea);
+        gameBox.getChildren().add(chatTextAreaScroll);
+        
+        TextField chat = new TextField();
+        gameBox.getChildren().add(chat);
+        
+        
+        
+        Scene gameScene = new Scene(gameBox,100,100); 
+        gameStage.setScene(gameScene);
+        gameStage.show();
+                
+    }
+}
---
+++ src/xp_fall_2015_gruppea/GuiMain.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xp_fall_2015_gruppea;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Elinor-skole
+ */
+public class GuiMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        VBox logOnBox = new VBox(5);
+
+        VBox userBox = new VBox();
+        Label stringUser = new Label("User");
+        TextField user = new TextField();
+        userBox.getChildren().add(stringUser);
+        userBox.getChildren().add(user);
+        
+        VBox passwordBox = new VBox();
+        Label stringPasswordBox = new Label("Password");
+        PasswordField password = new PasswordField();
+        passwordBox.getChildren().add(stringPasswordBox);
+        passwordBox.getChildren().add(password);
+        
+        logOnBox.getChildren().add(userBox);
+        logOnBox.getChildren().add(passwordBox);
+ 
+        
+        Button logOn = new Button();
+        logOn.setText("log on");
+        logOnBox.getChildren().add(logOn);  
+        logOn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                if(true){
+                    GameBoard game = new GameBoard();
+                }else{
+                  Stage notFoundStage = new Stage();
+                  notFoundStage.setTitle("Error");
+                  Label errorMessage = new Label("Error");
+                  HBox errorBox = new HBox();
+                  errorBox.getChildren().add(errorMessage);
+                  Scene notFoundScrene = new Scene(errorBox, 125, 100);
+                  notFoundStage.setScene(notFoundScrene);
+                  notFoundStage.show();
+                }
+                
+
+            }
+        });
+        
+      
+        Scene scene = new Scene(logOnBox, 300, 250);
+        primaryStage.setTitle("log on");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
