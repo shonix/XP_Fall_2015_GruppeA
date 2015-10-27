b55a197d2693194081e40655361aab44fc9f3a58
pet1100 <peterbrj@gmail.com>
2015-10-27 11:56:49 +0100

    DB access smaller change

--- src/DBAccess/DBGameStat.java
+++ src/DBAccess/DBGameStat.java
@@ -2,13 +2,52 @@ package DBAccess;
 
 import Server.User;
 
+import java.sql.SQLException;
+
 /**
  * Created by Peter on 27-10-2015.
  */
-public class DBGameStat
+public class DBGameStat extends DBElement
 {
-    protected void changeGame()
+    protected void changeGame(String type, char winLoseDraw, User u)
     {
+        String statIncrease = "";
+        try
+        {
+            switch (winLoseDraw)
+            {
+                case 'w':
+                    statIncrease = "win";
+                    break;
+                case 'l':
+                    statIncrease = "loss";
+                    break;
+                case 'd':
+                    statIncrease = "draw";
+                    break;
+                default:
+                    return;
+            }
+            createConnection();
+            String query = "UPDATE mydb.leaves (" + statIncrease + ") VALUES (?) where ID = " + u.getID();
+            preparedStatement = connect.prepareStatement(query);
 
+//            preparedStatement.setInt();
+
+            preparedStatement.execute();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        closeConnection();
     }
 }
