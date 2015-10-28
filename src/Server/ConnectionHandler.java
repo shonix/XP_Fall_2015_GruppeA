36b5d86e8fa1fe2103b68d1658b0c023d8f864e7
Shonix <Peterfut123@gmail.com>
2015-10-28 09:43:31 +0100

    Oprettet statisk liste med connection sockets. Udskriver en besked til alle connection sockets, når chat besked modtages.

--- src/Server/ConnectionHandler.java
+++ src/Server/ConnectionHandler.java
@@ -2,15 +2,18 @@ package Server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ConnectionHandler {
 	ServerSocket socketListener = new ServerSocket(7776);
+	public static ArrayList<Socket> allUsers = new ArrayList<>();
 	public ConnectionHandler() throws Exception {
 		while (true) {
 			Socket userSocket = socketListener.accept();
 			if (userSocket != null) {
 				System.out.println("Trying to connect");
-				ServerConnection conn = new ServerConnection(userSocket);
+				ServerConnection conn = new ServerConnection(userSocket,this);
+				allUsers.add(userSocket);
 				System.out.println("Connection established");
 				conn.start();
 			}
