9f9bf47def78d36f43c073d7ca9d406fbfe9d691
pet1100 <peterbrj@gmail.com>
2015-10-30 09:25:28 +0100

    Added description to inputcheck

--- src/InputCheck.java
+++ src/InputCheck.java
@@ -1,22 +1,25 @@
 import java.util.regex.Pattern;
 
 /**
- *
  * @author Markus
  */
+
 public class InputCheck
 {
 
-    private static final Pattern patternIP = Pattern.compile(
-            "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
-    private static final Pattern patternPort = Pattern.compile(
-            "\\d+");
-    private static final Pattern patternUser = Pattern.compile(
-            "^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
-    private static final Pattern patternPassword = Pattern.compile(
-            "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$");
+    private static final Pattern patternIP = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+    private static final Pattern patternPort = Pattern.compile("\\d+");
+    private static final Pattern patternUser = Pattern.compile("^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
+    private static final Pattern patternPassword = Pattern.compile("^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$");
 
-    //Call with inputType as IP, Port, User, Password or Message
+ /*
+ *         InputCheck chackes the input if it is valid.
+ *         IP must follow the IP standard format. (eg. 192.168.1.1)
+ *         Port number must be between 2000 - 65000
+ *         User name must be between 6 - 12 characters long, and can't contain special characters.
+ *         Password must contain both letters and numbers, and must be between 8 - 16 characters long.
+ *         Messege must be longer then 0.
+ */
     public static boolean isValid(String inputType, String inputValue)
     {
         boolean valid = false;
@@ -75,7 +78,7 @@ public class InputCheck
         }
         return valid;
     }
-    
+
     //must contain both letters and numbers, must be 8-16 characters long an must not contain special characters
     private static boolean checkPassword(String input)
     {
